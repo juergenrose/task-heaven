4a1b1b80b3fe2045b4f4ef6a50de99d2064a1c5a
Jürgen M <j.mosgan@gmail.com>
2024-03-19 16:02:43 +0100

    added Profile model for images

--- task_heaven/tasks/models.py
+++ task_heaven/tasks/models.py
@@ -6,5 +6,10 @@ class Task(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField(max_length=1000)
     date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, max_length=100, on_delete=models.CASCADE, null=True)
 
+
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(null=True, blank=True, default='default.png') 
     user = models.ForeignKey(User, max_length=100, on_delete=models.CASCADE, null=True)
