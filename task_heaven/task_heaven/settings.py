8b7a8e9fc8b30452addbe0e47dad1880e86d76f1
Jürgen M <j.mosgan@gmail.com>
2024-03-19 16:02:11 +0100

    added media folder in settings.py, urls.py and admin.py

--- task_heaven/task_heaven/settings.py
+++ task_heaven/task_heaven/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
